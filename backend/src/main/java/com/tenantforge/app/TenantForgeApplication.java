06ac517cdf6abc3bca076ad530747106ec39d1f1
lzh <lzh840723@hotmail.com>
2025-10-04 01:33:28 +0900

    chore: log platform port diagnostics

--- backend/src/main/java/com/tenantforge/app/TenantForgeApplication.java
+++ backend/src/main/java/com/tenantforge/app/TenantForgeApplication.java
@@ -3,12 +3,18 @@ package com.tenantforge.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class TenantForgeApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(TenantForgeApplication.class);
+
     public static void main(String[] args) {
+        log.info("Detected PORT env: {}", System.getenv("PORT"));
+        log.info("Effective server.port property: {}", System.getProperty("server.port"));
         SpringApplication.run(TenantForgeApplication.class, args);
     }
 }
